e86c2d0237731014665fb8a503bae3fdf04761b1
Princekumar <99081875+PrinceDobariya0710@users.noreply.github.com>
2025-01-16 23:24:48 +0530

    datatypes and ECMA

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use script"; //treat all JS code in a file as newer version
+
+//alert(3+1) //we are using nodejs , not browser
+
+console.log(3+2); //code readabilty should be high
+console.log("Prince")
+
+//mdn
+//https://tc39.es/ecms262
+
+//datatypes
+
+// number => 2^ power 53
+// bigint
+// float
+// string=> ""
+// boolean => true/false
+// null =>  object, it is itself stand-alone value. it's type itself. It is a representation of an empty value. it is value.
+// undefined => memory is allocated but value is not there.
+// symbol => uniqueness
+
+//  object => 
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) //object
