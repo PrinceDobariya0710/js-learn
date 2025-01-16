b303f1d622b8e7a0907e96579798d1eb8f52ad57
Princekumar <99081875+PrinceDobariya0710@users.noreply.github.com>
2025-01-16 23:38:02 +0530

    conversion of DataTypes

---
+++ 01_basics/03_converstionOperation.js
@@ -0,0 +1,30 @@
+let score = "123A"
+
+console.log(typeof(score)) //string
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber) //NaN
+
+// "33" -> 33
+// "33abs" => NaN=> number
+// true=>1 ; false=>0
+
+let isLoggedIn = 0
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanisLoggedIn)
+
+// 1=> true
+// 0 => false
+// "" => false
+// "prince" => true
+
+let someNumber = 33
+let stringNumber =  String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
