ea9fb7eb1470db5ec42baa83bfe43e039e87248d
Princekumar <99081875+PrinceDobariya0710@users.noreply.github.com>
2025-01-17 23:06:48 +0530

    datatypes heap and stack

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,3 +35,21 @@ const test2 = function(){
     console.log("Hellow World")
 }
 
+// Memory Types
+// Stack (Primitive) - You get a Copy, Heap (Non-Primitive)- you get a reference
+
+let myName = "prince"
+let anotherName = myName
+anotherName = "test"
+console.log(anotherName)
+console.log(myName)
+
+let user = {
+    email : "p@mail.com",
+    upi:"test@ybl"
+}
+let user2 = user
+user2.email="pp@mail.om"
+console.log(user)
+console.log(user2)
+
