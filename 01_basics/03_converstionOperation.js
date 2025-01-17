83fd9118674c8e3e4c79e1d1957e38804558ba9e
Princekumar <99081875+PrinceDobariya0710@users.noreply.github.com>
2025-01-17 22:35:22 +0530

    comparison

--- 01_basics/03_converstionOperation.js
+++ 01_basics/03_converstionOperation.js
@@ -27,4 +27,47 @@ let stringNumber =  String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
 
+// *********************Operations****************
+
+let value=2
+let negValue= -value
+// console.log(negValue)
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+
+let str1="hello"
+let str2=" Worls"
+let str3 = str1+str2
+console.log(str3)
+
+// Never write like this
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(2+3+"1") 
+
+console.log((3+4)*5%3)
+
+console.log(+true)
+console.log(+"")
+
+let gameCounter=22
+gameCounter++;
+console.log(gameCounter)
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
 
