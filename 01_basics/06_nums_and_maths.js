c5a44e7a43a2b9a25fc215f6cc9f9a6cab4b10d9
PrinceDobariya0710 <99081875+PrinceDobariya0710@users.noreply.github.com>
2025-01-18 11:36:13 +0530

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,18 @@
+const hundreads = 100000
+// console.log(hundreads.toLocaleString('en-IN'))
+
+// ******Maths*****
+// console.log(Math)
+// console.log(Math.abs(-2))
+// console.log(Math.round(4.7))
+// console.log(Math.floor(4.99))
+// console.log(Math.ceil(4.99))
+// console.log(Math.min(3,4,4,5,6,2,33))
+// console.log(Math.max(3,4,4,5,6,2,33))
+
+console.log(Math.floor(Math.random()*10) + 1) // Always between 0 and 1
+
+const min = 10
+const max = 20
+// TO generate random values within some range
+console.log(Math.floor((Math.random() * (max-min+1)) + min))
