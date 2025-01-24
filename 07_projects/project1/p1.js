1db5e7b284a57a84face0db163d597ecd08940a8
PrinceDobariya0710 <99081875+PrinceDobariya0710@users.noreply.github.com>
2025-01-24 22:21:31 +0530

    project1 js

---
+++ 07_projects/project1/p1.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll(".button")
+console.log(buttons);
+const body = document.querySelector('body')
+buttons.forEach((button)=>{
+    console.log(button);
+    button.addEventListener('click',(e)=>{
+        console.log(e.target.id);
+        body.style.backgroundColor = e.target.id
+    })
+})
